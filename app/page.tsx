0b6007fffca8ee1bdb4a598a47785dc7a35277f2
Ivan <i.pabayo3000@gmail.com>
2025-11-20 00:44:07 +0800

    INITIAL commit. v1: already functional gps tracker. db to be added

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,81 @@
-import Image from "next/image";
+// "use client";
+
+// import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+// import "leaflet/dist/leaflet.css";
+
+// export default function Home() {
+//   console.log(window);
+//   const height = window.innerHeight;
+
+//   return (
+//     <div className={`h-[${height}]`}>
+//       <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+//         <TileLayer
+//           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//         />
+//         <Marker position={[51.505, -0.09]}>
+//           <Popup>
+//             A pretty CSS3 popup. <br /> Easily customizable.
+//           </Popup>
+//         </Marker>
+//       </MapContainer>
+//     </div>
+//   );
+// }
+
+// "use client";
+
+// import { useEffect, useState } from "react";
+// import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+// import "leaflet/dist/leaflet.css";
+
+// export default function Home() {
+//   const [height, setHeight] = useState(400); // default fallback height
+
+//   useEffect(() => {
+//     setHeight(window.innerHeight);
+//   }, []);
+
+//   return (
+//     <div style={{ height }}>
+//       <MapContainer
+//         // style={{ height: "100%", width: "100%" }}
+//         center={[51.505, -0.09]}
+//         zoom={13}
+//         scrollWheelZoom={false}
+//       >
+//         <TileLayer
+//           attribution="&copy; OpenStreetMap contributors"
+//           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+//         />
+//         <Marker position={[51.505, -0.09]}>
+//           <Popup>A pretty CSS3 popup.</Popup>
+//         </Marker>
+//       </MapContainer>
+//     </div>
+//   );
+// }
+
+"use client";
+
+import { useState, useEffect } from "react";
+
+import dynamic from "next/dynamic";
+
+const Map = dynamic(() => import("../components/Map"), {
+  ssr: false,
+});
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+  // return <Map center={latlng} />;
+  return <Map />;
 }
+
+export type Tcenter = {
+  lat: number;
+  lng: number;
+};
+
+// lat: 14.125451039620254,
+// lng: 121.14177005696203,
---
+++ components/Map.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
+// import L from "leaflet";
+// import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+// import markerIcon from "leaflet/dist/images/marker-icon.png";
+// import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+// delete (L.Icon.Default.prototype as any)._getIconUrl;
+
+// L.Icon.Default.mergeOptions({
+//   iconRetinaUrl: markerIcon2x.src,
+//   iconUrl: markerIcon.src,
+//   shadowUrl: markerShadow.src,
+// });
+
+import L from "leaflet";
+import iconRetinaUrl from "leaflet/dist/images/marker-icon-2x.png";
+import iconUrl from "leaflet/dist/images/marker-icon.png";
+import shadowUrl from "leaflet/dist/images/marker-shadow.png";
+
+import { Tcenter } from "@/app/page";
+import { useState, useEffect } from "react";
+
+const DefaultIcon = L.icon({
+  iconRetinaUrl: iconRetinaUrl as any,
+  iconUrl: iconUrl as any,
+  shadowUrl: shadowUrl as any,
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+});
+
+L.Marker.prototype.options.icon = DefaultIcon;
+
+function AutoFollow({ position }: { position: Tcenter }) {
+  const map = useMap();
+
+  useEffect(() => {
+    if (!position) return;
+    map.setView(position); // Auto center
+  }, [position]);
+
+  return null;
+}
+
+// export default function Map({ center }: { center: Tcenter }) {
+export default function Map() {
+  // const map = useMap();
+
+  const [latlng, setLatLng] = useState({
+    lat: 0,
+    lng: 0,
+  });
+
+  function showPosition(position: any) {
+    setLatLng({
+      lat: position.coords.latitude,
+      lng: position.coords.longitude,
+    });
+    // console.log(latlng.lat);
+    // console.log(latlng.lng);
+  }
+
+  useEffect(() => {
+    const logInterval = setInterval(() => {
+      // navigator.geolocation.getCurrentPosition(showPosition);
+      navigator.geolocation.watchPosition(showPosition);
+      // }, 1500);
+    }, 10);
+
+    return () => {
+      clearInterval(logInterval);
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log(`Updated GPS: Lat: ${latlng.lat}, Lng: ${latlng.lng}`);
+    // map.setView([latlng.lat, latlng.lng]);
+  }, [latlng]);
+
+  return (
+    <div>
+      <MapContainer
+        // center={[center.lat, center.lng]}
+        center={[latlng.lat, latlng.lng]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {/* <Marker position={[center.lat, center.lng]}> */}
+
+        <Marker position={[latlng.lat, latlng.lng]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+        {latlng && <AutoFollow position={latlng} />}
+      </MapContainer>
+      <div className="p-4">
+        <p>Geo Lat: {latlng.lat}</p>
+        <p>Geo Lng: {latlng.lng}</p>
+      </div>
+    </div>
+  );
+}
